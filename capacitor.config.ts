337833e7f04690db2fc5cad39f84bd703f35e5a0
Filip Cucakovic <filip.cucakovic@gmail.com>
2025-04-26 17:44:37 +0200

    Add Android platform after Capacitor init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shoulderelbow.app',
+  appName: 'ShoulderElbowSerbia',
+  webDir: 'www'
+};
+
+export default config;
