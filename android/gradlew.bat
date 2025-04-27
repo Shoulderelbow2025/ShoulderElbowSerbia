e7a06b1f929ab8717021f793c4e2364192eb9ba9
Shoulderelbow2025 <filip.cucakovic@gmail.com>
2025-04-27 09:02:39 +0200

    Add files via upload

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,2 +1,21 @@
 @echo off
-gradlew %*
+set DIR=%~dp0
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIR%
+set DEFAULT_JVM_OPTS=
+
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+"%JAVA_EXE%" -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+exit /b 1
+
+:findJavaFromJavaHome
+set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+:init
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
