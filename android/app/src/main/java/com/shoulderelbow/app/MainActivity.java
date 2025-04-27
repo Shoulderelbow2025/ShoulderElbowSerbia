77579c36af2d78aa647e380b9ddd6abd3aa8a509
Filip Cucakovic <filip.cucakovic@gmail.com>
2025-04-27 10:53:01 +0200

    Added correct gradlew and gradle wrapper files

--- android/app/src/main/java/com/shoulderelbow/app/MainActivity.java
+++ android/app/src/main/java/com/shoulderelbow/app/MainActivity.java
@@ -1,11 +1,5 @@
 package com.shoulderelbow.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
